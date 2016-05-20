8b73f44001058ba190fa3a900cb30b80d96f0a41
Luke Floden <robodomo1@gmail.com>
2016-05-20 08:53:47 -0400

    update pins and changed mode to semiauto

--- stratum-loop.ino
+++ stratum-loop.ino
@@ -1,12 +1,14 @@
 #include "SparkIntervalTimer/SparkIntervalTimer.h"
 #include "SoftPWM/SoftPWM.h"
 
-uint8_t chan[3][3] = {{D0, D2, D4}, {D6, A1, A2}, {A3, A4, A5}}; 
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+uint8_t chan[3][3] = {{D5, D4, D1}, {D0, A5, A4}, {WKP, A3, A2}}; 
 uint8_t state[3][3];
 
 void setup() {
+  pinMode(D8, INPUT);
   SoftPWMBegin();                     //begin softPWM and set fade time
-
   for (int i = 0; i < 3; i++){        //set pwm pins as outputs
     for(int c = 0; c < 3; c++)
     {
@@ -14,13 +16,13 @@ void setup() {
       SoftPWMSet(chan[i][c], state[i][c] = 0);
     }
   }
-
   SoftPWMSetFadeTime(ALL,100,100);
 }
-
 void loop(){
-  randomSeed(analogRead(A0));
-
+  if (D8 == HIGH) {
+    Particle.connect();
+  } else {
+    randomSeed(analogRead(A0));
   for (int i = 0; i < 3; i++){        //set pwm pins as outputs
     for(int c = 0; c < 3; c++)
     {
@@ -31,6 +33,6 @@ void loop(){
         state[2-i][c] = random(255);
     }
   }
-
-  delay(10000);
+  delay(2000);
+ }
 }
